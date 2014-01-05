9c492297c64adec92d9dd5e8874818b3f7caf715
Yanone <post@yanone.de>
2014-01-05 05:54:38 -0800

    Initial commit.

---
+++ generators/base.py
@@ -0,0 +1,32 @@
+########################################
+# generator definitions
+
+class BaseGeneratorDefinition:
+	def __init__(self):
+		self.canvas = None
+
+
+	def Rect(self, o = None):
+		if self.canvas.strict: raise NotImplementedError
+
+	def BezierPathBegin(self, o = None):
+		if self.canvas.strict: raise NotImplementedError
+
+	def BezierPathEnd(self, o = None):
+		if self.canvas.strict: raise NotImplementedError
+
+	def BezierPathMoveTo(self, o = None):
+		if self.canvas.strict: raise NotImplementedError
+
+	def BezierPathLineTo(self, o = None):
+		if self.canvas.strict: raise NotImplementedError
+
+	def BezierPathCurveTo(self, o = None):
+		if self.canvas.strict: raise NotImplementedError
+
+	def BezierPathClosePath(self, o = None):
+		if self.canvas.strict: raise NotImplementedError
+
+	def Generate(self):
+		pass
+
